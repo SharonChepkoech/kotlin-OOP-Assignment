68ccb6896c100381c2c2e72b9f1f8c26cbf3e5bf
Sharon Chepkoech <sharoncchepkoech@gmail.com>
2022-03-31 17:39:07 +0300

    corretions

--- src/main.kt
+++ src/main.kt
@@ -17,7 +17,10 @@ fun main() {
         identity.eat(5)
         println(identity.weight)
         identity.Speak("I love coding in Kotlin")
+//        identity.Birthday()
+        println(identity.age)
         identity.Birthday()
+        println(identity.age)
         var Sharon = Human("Sharon","Chepkoech","chekochepkoech@gmail.com",7273737,"hush7827@!")
         println(Sharon.firstName.toString()+ " " +Sharon.email)
 
@@ -33,10 +36,16 @@ class Person(var name:String, var age:Int, var weight:Double) {
 
         }
         fun Birthday(){
-               age = age++
-                println(age)
+//               age = age++
+//                println(age)
+                age++
+//                you use ++ if the change will be permanent, and the number can't say the same after the addition such as age.
+//                it is not completely necessary that you println.
         }
 }
 data class Human(var firstName: String, var secondName: String, var email: String, var phoneNumber:Int, var passsword: Any){
 
 }
+//a data class has no body ({})
+//a data clas is just holds values performs no operations
+// a phone number should be stored in
